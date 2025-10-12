c13086720b503bb89d059f6653e267f8a72d7478
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-10-12 23:46:17 +1100

    Auto-commit: modified 1 file

--- bin/wallpaper-picker.sh
+++ bin/wallpaper-picker.sh
@@ -5,5 +5,5 @@ selected=$(yazi --chooser-file=/dev/stdout ~/wallpapers)
 
 # If a file was selected, set it as wallpaper with swww
 if [ -n "$selected" ]; then
-    swww img "$selected"
+    swww img "$selected" --resize fit
 fi
