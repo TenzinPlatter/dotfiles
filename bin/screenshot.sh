82cafdfa01fb34ecf191df1e9ea614cff7418c63
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-10-15 13:12:29 +1100

    Auto-commit: modified 1 file

--- bin/screenshot.sh
+++ bin/screenshot.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
-grim -g "$(slurp)" - | satty -f -
+
+# Create Screenshots directory if it doesn't exist
+mkdir -p ~/Pictures/Screenshots
+
+# Generate filename with date and time (e.g., screenshot_2025-10-15_14-30-45.png)
+FILENAME=~/Pictures/Screenshots/screenshot_$(date +%Y-%m-%d_%H-%M-%S).png
+
+# Take screenshot and open in satty with output filename
+grim -g "$(slurp)" - | satty -f - --output-filename "$FILENAME"
