0e1b8dc39075c1a6112fa1144b344d843d156674
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-10-29 14:06:37 +1100

    Auto-commit: modified 1 file

--- bin/screenshot.sh
+++ bin/screenshot.sh
@@ -6,5 +6,10 @@ mkdir -p ~/Pictures/Screenshots
 # Generate filename with date and time (e.g., screenshot_2025-10-15_14-30-45.png)
 FILENAME=~/Pictures/Screenshots/screenshot_$(date +%Y-%m-%d_%H-%M-%S).png
 
-# Take screenshot and open in satty with output filename
-grim -g "$(slurp)" - | satty -f - --output-filename "$FILENAME"
+# Take screenshot, copy original to clipboard, and open in satty
+grim -g "$(slurp)" - | tee >(wl-copy --type image/png) | satty -f - --output-filename "$FILENAME"
+
+# After satty closes, if the annotated file was saved, update clipboard with it
+if [ -f "$FILENAME" ]; then
+    wl-copy --type image/png < "$FILENAME"
+fi
