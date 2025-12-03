302c1231c36201160a6f98e6fa3045cb23c7a173
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-12-04 10:54:33 +1100

    Modified 1
    Modified:
      bin/update_release.py

--- bin/update_release.py
+++ bin/update_release.py
@@ -1,9 +1,28 @@
 #!/usr/bin/env python3
 
 import os
+from pathlib import Path
+import yaml
 
 def main():
-    cwd = os.getcwd()
+    cwd = Path(os.getcwd())
+    release_files = []
+    for subdir in cwd.glob("./*"):
+        if not subdir.is_dir():
+            continue
+
+        release = subdir / ".github" / "workflows" / "release.yml"
+        if not release.is_file():
+            print(f"Subdir: {subdir} does not contain a release file")
+            continue
+
+        release_files.append(subdir)
+
+    for r in release_files:
+        with open(r, "+") as f:
+            contents = yaml.parse(f.read())
+
+        
 
 if __name__ == "__main__":
     main()
