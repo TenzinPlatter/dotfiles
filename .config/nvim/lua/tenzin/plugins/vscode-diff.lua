820aa929e1cac127066b86618605bd2ecab3789b
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-12-18 10:15:18 +1100

    Modified 1
    Modified:
      .config/nvim/lua/tenzin/plugins/vscode-diff.lua

--- .config/nvim/lua/tenzin/plugins/vscode-diff.lua
+++ .config/nvim/lua/tenzin/plugins/vscode-diff.lua
@@ -2,4 +2,13 @@ return {
   "esmuellert/vscode-diff.nvim",
   branch = "next",
   dependencies = { "MunifTanjim/nui.nvim" },
+  keys = {
+    {
+      "<leader>cd",
+      function ()
+        vim.cmd("CodeDiff")
+      end,
+      desc = "Toggle VSCode git diff"
+    }
+  }
 }
