57756bd5f448046ad55301850840677477be6c1e
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-12-20 18:20:40 +1100

    Modified 1
    Modified:
      .config/nvim/lua/tenzin/plugins/copilot.lua

--- .config/nvim/lua/tenzin/plugins/copilot.lua
+++ .config/nvim/lua/tenzin/plugins/copilot.lua
@@ -49,11 +49,11 @@ return {
 			})
 
 			-- Set keymaps manually
-			vim.keymap.set('i', '<C-J>', function()
+			vim.keymap.set('i', '<C-Space>', function()
 				require('copilot.suggestion').accept()
 			end, { desc = 'Accept Copilot suggestion' })
 
-			vim.keymap.set({'i', 'n'}, '<C-Y>', function()
+			vim.keymap.set({'i', 'n'}, '<M-L>', function()
 				require('copilot-lsp.nes').apply_pending_nes()
 				require('copilot-lsp.nes').walk_cursor_end_edit()
 			end, { desc = 'Accept and goto NES suggestion' })
