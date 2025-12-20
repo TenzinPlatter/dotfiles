7357620c5c932419f8ed47d889b298212a3c1c80
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-12-20 17:45:08 +1100

    Modified 1
    Modified:
      .config/nvim/lua/tenzin/plugins/treesitter.lua

--- .config/nvim/lua/tenzin/plugins/treesitter.lua
+++ .config/nvim/lua/tenzin/plugins/treesitter.lua
@@ -1,32 +1,5 @@
 return {
-	'https://github.com/nvim-treesitter/nvim-treesitter',
-	dependencies = {
-		"https://github.com/nvim-treesitter/nvim-treesitter-textobjects",
-		"https://github.com/nvim-treesitter/nvim-treesitter-context",
-	},
+	"https://github.com/nvim-treesitter/nvim-treesitter",
 	lazy = false,
 	build = ":TSUpdate",
-	config = function()
-		require( 'nvim-treesitter.configs' ).setup({
-		  ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query" },
-
-		  sync_install = false,
-
-		  auto_install = true,
-
-		  highlight = {
-		    enable = true,
-
-		    additional_vim_regex_highlighting = false,
-		  },
-
-			matchup = {
-				enable = true,
-			}
-		})
-
-		require('treesitter-context').setup({
-			enable = false,
-		})
-	end
 }
