c8860784e9bf1e46ab18132903258854b06b1c87
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-12-21 22:32:55 +1100

    Modified 1
    Modified:
      .config/nvim/lua/tenzin/plugins/treesitter.lua

--- .config/nvim/lua/tenzin/plugins/treesitter.lua
+++ .config/nvim/lua/tenzin/plugins/treesitter.lua
@@ -3,12 +3,4 @@ return {
 	lazy = false,
 	branch = "main",
 	build = ":TSUpdate",
-	config = function()
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = { "yaml", "lua", "rust", "javascript", "typescript", "python", "bash", "json", "toml", "markdown" },
-			callback = function()
-				vim.treesitter.start()
-			end,
-		})
-	end,
 }
