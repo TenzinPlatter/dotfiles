30004c3f02e1a26dc7af7bdb171972d9c029aa6e
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2026-01-06 09:30:57 +1100

    Modified 1
    Modified:
      .config/nvim/lua/tenzin/plugins/treesitter.lua

--- .config/nvim/lua/tenzin/plugins/treesitter.lua
+++ .config/nvim/lua/tenzin/plugins/treesitter.lua
@@ -1,5 +1,26 @@
 return {
 	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				auto_install = true,
+
+				-- Enable syntax highlighting
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+				},
+
+				-- Enable indentation
+				indent = {
+					enable = true,
+				},
+			})
+		end,
+	},
+	{
 		"nvim-treesitter/nvim-treesitter-textobjects",
 		branch = "main",
 		dependencies = { "nvim-treesitter/nvim-treesitter", branch = "main" },
