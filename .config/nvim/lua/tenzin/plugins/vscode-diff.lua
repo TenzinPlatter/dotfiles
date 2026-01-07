2a9c47fa9dee6fe0c5b8cfaa4a01242cf98b1af0
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2026-01-07 11:56:32 +1100

    Deleted 1, Modified 1
    Deleted:
      .config/nvim/lua/tenzin/plugins/bento.lua
    Modified:
      .config/nvim/lua/tenzin/plugins/vscode-diff.lua

--- .config/nvim/lua/tenzin/plugins/vscode-diff.lua
+++ .config/nvim/lua/tenzin/plugins/vscode-diff.lua
@@ -9,6 +9,13 @@ return {
 			desc = "Open prompt for VSCode-diff view",
 		},
 		{
+			"<leader>cdm",
+			function()
+				vim.cmd("CodeDiff main")
+			end,
+			desc = "Open prompt for VSCode-diff view",
+		},
+		{
 			"<leader>cdf",
 			":CodeDiff file",
 			desc = "Open prompt for VSCode-diff view",
