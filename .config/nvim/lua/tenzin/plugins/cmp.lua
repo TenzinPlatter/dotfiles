275820a1d85126df855f39967e69081c94ba13c3
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2026-01-10 13:07:30 +1100

    Modified 2
    Modified:
      .config/nvim/lua/tenzin/plugins/blink-cmp.lua
      .config/nvim/lua/tenzin/plugins/cmp.lua

--- .config/nvim/lua/tenzin/plugins/cmp.lua
+++ .config/nvim/lua/tenzin/plugins/cmp.lua
@@ -1,81 +1,83 @@
-return {
-	{
-		"https://github.com/hrsh7th/nvim-cmp",
-		version = "v2.3.0",
-		dependencies = {
-			"https://github.com/hrsh7th/cmp-nvim-lsp",
-			"https://github.com/L3MON4D3/LuaSnip",
-			"https://github.com/saadparwaiz1/cmp_luasnip",
-			"https://github.com/rafamadriz/friendly-snippets",
-			"tranzystorekk/cmp-minikind.nvim",
-			{
-				"https://github.com/nvim-mini/mini.icons",
-				verions = false,
-				config = function()
-					require("mini.icons").setup()
-				end,
-			}
-		},
-		config = function()
-			local cmp = require("cmp")
-			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+return {}
 
-			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+-- return {
+-- 	{
+-- 		"https://github.com/hrsh7th/nvim-cmp",
+-- 		version = "v2.3.0",
+-- 		dependencies = {
+-- 			"https://github.com/hrsh7th/cmp-nvim-lsp",
+-- 			"https://github.com/L3MON4D3/LuaSnip",
+-- 			"https://github.com/saadparwaiz1/cmp_luasnip",
+-- 			"https://github.com/rafamadriz/friendly-snippets",
+-- 			"tranzystorekk/cmp-minikind.nvim",
+-- 			{
+-- 				"https://github.com/nvim-mini/mini.icons",
+-- 				verions = false,
+-- 				config = function()
+-- 					require("mini.icons").setup()
+-- 				end,
+-- 			}
+-- 		},
+-- 		config = function()
+-- 			local cmp = require("cmp")
+-- 			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
-			local luasnip = require("luasnip")
-			require("luasnip.loaders.from_lua").lazy_load({ paths = { vim.fn.stdpath("config") .. "/luasnippets" } })
-			require("luasnip.loaders.from_vscode").lazy_load()
+-- 			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
+-- 			local luasnip = require("luasnip")
+-- 			require("luasnip.loaders.from_lua").lazy_load({ paths = { vim.fn.stdpath("config") .. "/luasnippets" } })
+-- 			require("luasnip.loaders.from_vscode").lazy_load()
 
-				formatting = {
-					format = require("cmp-minikind").cmp_format(),
-				},
+-- 			cmp.setup({
+-- 				snippet = {
+-- 					expand = function(args)
+-- 						require("luasnip").lsp_expand(args.body)
+-- 					end,
+-- 				},
 
-				mapping = cmp.mapping.preset.insert({
-					["<C-u>"] = cmp.mapping.scroll_docs(-4),
-					["<C-d>"] = cmp.mapping.scroll_docs(4),
-					["<Tab>"] = cmp.mapping.confirm({ select = true }),
+-- 				formatting = {
+-- 					format = require("cmp-minikind").cmp_format(),
+-- 				},
 
-					["<C-L>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.mapping.confirm({ select = true })
-						elseif luasnip.expand_or_jumpable() then
-							luasnip.expand_or_jump()
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
+-- 				mapping = cmp.mapping.preset.insert({
+-- 					["<C-u>"] = cmp.mapping.scroll_docs(-4),
+-- 					["<C-d>"] = cmp.mapping.scroll_docs(4),
+-- 					["<Tab>"] = cmp.mapping.confirm({ select = true }),
 
-					["<C-H>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.mapping.confirm({ select = true })
-						elseif luasnip.jumpable(-1) then
-							luasnip.jump(-1)
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-				}),
+-- 					["<C-L>"] = cmp.mapping(function(fallback)
+-- 						if cmp.visible() then
+-- 							cmp.mapping.confirm({ select = true })
+-- 						elseif luasnip.expand_or_jumpable() then
+-- 							luasnip.expand_or_jump()
+-- 						else
+-- 							fallback()
+-- 						end
+-- 					end, { "i", "s" }),
 
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-				}, {
-					{ name = "buffer" },
-				}),
-			})
+-- 					["<C-H>"] = cmp.mapping(function(fallback)
+-- 						if cmp.visible() then
+-- 							cmp.mapping.confirm({ select = true })
+-- 						elseif luasnip.jumpable(-1) then
+-- 							luasnip.jump(-1)
+-- 						else
+-- 							fallback()
+-- 						end
+-- 					end, { "i", "s" }),
+-- 				}),
 
-			cmp.setup.cmdline(":", {
-				sources = cmp.config.sources({
-					{ name = "fuzzy_path" },
-				}),
-			})
-		end,
-	},
-}
+-- 				sources = cmp.config.sources({
+-- 					{ name = "nvim_lsp" },
+-- 					{ name = "luasnip" },
+-- 				}, {
+-- 					{ name = "buffer" },
+-- 				}),
+-- 			})
+
+-- 			cmp.setup.cmdline(":", {
+-- 				sources = cmp.config.sources({
+-- 					{ name = "fuzzy_path" },
+-- 				}),
+-- 			})
+-- 		end,
+-- 	},
+-- }
