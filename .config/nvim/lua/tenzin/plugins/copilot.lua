d310b7cb041a149db06c84de91bec2896f965b8d
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2026-01-11 19:33:46 +1100

    Deleted 2, Modified 2
    Deleted:
      .config/kitty/hyde.conf
      .config/kitty/rose-pine-moon.conf
    Modified:
      .config/kitty/kitty.conf
      .config/nvim/lua/tenzin/plugins/copilot.lua

--- .config/nvim/lua/tenzin/plugins/copilot.lua
+++ .config/nvim/lua/tenzin/plugins/copilot.lua
@@ -3,33 +3,40 @@ return {
 		"zbirenbaum/copilot.lua",
 		cmd = "Copilot",
 		event = "InsertEnter",
-		config = function()
-			require("copilot").setup({
-				suggestion = {
-					auto_trigger = true,
-					keymap = {
-						accept = false,
-					},
+		opts = {
+			suggestion = {
+				auto_trigger = true,
+				keymap = {
+					accept = "<C-Space>",
+					accept_word = "<C-Right>",
+					accept_line = "<C-L>",
 				},
-				nes = {
-					enabled = false,
-					keymap = {
-						accept_and_goto = false,
-						accept = false,
-						dismiss = "<Esc>",
-					},
+			},
+			panel = {
+				enabled = false,
+			},
+			nes = {
+				enabled = false,
+				keymap = {
+					accept_and_goto = false,
+					accept = false,
+					dismiss = "<Esc>",
 				},
-			})
+			},
+		},
+		-- config = function()
+		-- 	require("copilot").setup({
+		-- 	})
 
-			-- Set keymaps manually
-			vim.keymap.set('i', '<C-Space>', function()
-				require('copilot.suggestion').accept()
-			end, { desc = 'Accept Copilot suggestion' })
+		-- 	-- Set keymaps manually
+		-- 	vim.keymap.set('i', '<C-Space>', function()
+		-- 		require('copilot.suggestion').accept()
+		-- 	end, { desc = 'Accept Copilot suggestion' })
 
-			vim.keymap.set({'i', 'n'}, '<M-L>', function()
-				require('copilot-lsp.nes').apply_pending_nes()
-				require('copilot-lsp.nes').walk_cursor_end_edit()
-			end, { desc = 'Accept and goto NES suggestion' })
-		end,
+		-- 	vim.keymap.set({'i', 'n'}, '<M-L>', function()
+		-- 		require('copilot-lsp.nes').apply_pending_nes()
+		-- 		require('copilot-lsp.nes').walk_cursor_end_edit()
+		-- 	end, { desc = 'Accept and goto NES suggestion' })
+		-- end,
 	},
 }
