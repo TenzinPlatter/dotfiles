24f9f52547614c0a387b23e0c3d205ba27267b72
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2026-01-11 20:44:50 +1100

    Modified 1, Added 1
    Modified:
      .config/nvim/lua/tenzin/plugins/copilot.lua
    Added:
      .config/tmux/

--- .config/nvim/lua/tenzin/plugins/copilot.lua
+++ .config/nvim/lua/tenzin/plugins/copilot.lua
@@ -3,41 +3,39 @@ return {
 		"zbirenbaum/copilot.lua",
 		cmd = "Copilot",
 		event = "InsertEnter",
-		opts = {
-			suggestion = {
-				auto_trigger = true,
-				enabled = true,
-				keymap = {
-					accept = "<C-Space>",
-					accept_word = "<C-Right>",
-					accept_line = "<C-L>",
+		config = function()
+			require("copilot").setup({
+				suggestion = {
+					auto_trigger = true,
+					enabled = true,
+					keymap = {
+						accept = "<C-Space>",
+						accept_word = "<C-Right>",
+						accept_line = "<C-L>",
+					},
 				},
-			},
-			panel = {
-				enabled = false,
-			},
-			nes = {
-				enabled = false,
-				keymap = {
-					accept_and_goto = false,
-					accept = false,
-					dismiss = "<Esc>",
+				panel = {
+					enabled = false,
 				},
-			},
-		},
-		-- config = function()
-		-- 	require("copilot").setup({
-		-- 	})
+				nes = {
+					enabled = false,
+					keymap = {
+						accept_and_goto = false,
+						accept = false,
+						dismiss = "<Esc>",
+					},
+				},
+			})
 
-		-- 	-- Set keymaps manually
-		-- 	vim.keymap.set('i', '<C-Space>', function()
-		-- 		require('copilot.suggestion').accept()
-		-- 	end, { desc = 'Accept Copilot suggestion' })
+			-- Set keymaps manually
+			vim.keymap.set("i", "<C-Space>", function()
+				require("copilot.suggestion").accept()
+			end, { desc = "Accept Copilot suggestion" })
 
-		-- 	vim.keymap.set({'i', 'n'}, '<M-L>', function()
-		-- 		require('copilot-lsp.nes').apply_pending_nes()
-		-- 		require('copilot-lsp.nes').walk_cursor_end_edit()
-		-- 	end, { desc = 'Accept and goto NES suggestion' })
-		-- end,
+			vim.keymap.set({ "i", "n" }, "<M-L>", function()
+				require("copilot-lsp.nes").apply_pending_nes()
+				require("copilot-lsp.nes").walk_cursor_end_edit()
+			end, { desc = "Accept and goto NES suggestion" })
+		end,
 	},
 }
