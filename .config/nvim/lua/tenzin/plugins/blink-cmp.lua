927b9e8232ca83e2bb8a7f7185f13c43b6b9e6b2
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2026-01-14 20:56:30 +1100

    Modified 1, Added 1
    Modified:
      .config/nvim/lua/tenzin/plugins/blink-cmp.lua
    Added:
      .config/opencode/

--- .config/nvim/lua/tenzin/plugins/blink-cmp.lua
+++ .config/nvim/lua/tenzin/plugins/blink-cmp.lua
@@ -40,6 +40,11 @@ return {
 		-- (Default) Only show the documentation popup when manually triggered
 		completion = { documentation = { auto_show = true } },
 
+		-- Disable snippet placeholder highlights
+		snippets = {
+			active_item = { enabled = false },
+		},
+
 		-- Default list of enabled providers defined so that you can extend it
 		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
@@ -48,7 +53,7 @@ return {
 
 		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
 		-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust_with_warning"`
 		--
 		-- See the fuzzy documentation for more information
 		fuzzy = { implementation = "prefer_rust_with_warning" },
