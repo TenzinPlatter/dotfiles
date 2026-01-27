c3c57af548bcc110517a070ce56e14e94bcf80a0
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2026-01-27 16:20:40 +1100

    Modified 1
    Modified:
      .config/zsh/user/programs/anaconda.zsh

--- .config/zsh/user/programs/anaconda.zsh
+++ .config/zsh/user/programs/anaconda.zsh
@@ -15,3 +15,20 @@ conda() {
     conda "$@"
 }
 # <<< conda lazy initialize <<<
+
+anaconda() {
+    unfunction anaconda
+    __conda_setup="$('/home/tenzin/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__conda_setup"
+    else
+        if [ -f "/home/tenzin/anaconda3/etc/profile.d/conda.sh" ]; then
+            . "/home/tenzin/anaconda3/etc/profile.d/conda.sh"
+        else
+            export PATH="/home/tenzin/anaconda3/bin:$PATH"
+        fi
+    fi
+    unset __conda_setup
+    anaconda "$@"
+}
+# <<< conda lazy initialize <<<
