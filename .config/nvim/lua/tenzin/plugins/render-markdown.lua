87c2cac64308fd56b64c7e63c05bb49bb2febc7b
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2026-01-28 17:38:55 +1100

    Modified 1, Added 1
    Modified:
      .config/nvim/lua/tenzin/plugins/markdown.lua
    Added:
      .config/nvim/lua/tenzin/plugins/render-markdown.lua

--- .config/nvim/lua/tenzin/plugins/render-markdown.lua
+++ .config/nvim/lua/tenzin/plugins/render-markdown.lua
@@ -2,13 +2,14 @@ return {
 	"https://github.com/MeanderingProgrammer/render-markdown.nvim",
 	config = function()
 		vim.api.nvim_set_hl(0, "RenderMarkdownCodeBorderSubtle", { bg = "#1a1a1a" })
+
 		require("render-markdown").setup({
 			bullet = {
 				right_pad = 1,
 			},
 			code = {
 				disable_background = true,
-				style = "normal",
+				style = "full",
 				border = "thick",
 				highlight_border = "RenderMarkdownCodeBorderSubtle",
 			},
