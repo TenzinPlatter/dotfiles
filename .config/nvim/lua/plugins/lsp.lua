86e0830a2ca06e59ea0b01c60b7980307ac2e91f
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2026-02-17 13:41:07 +1100

    Modified 2
    Modified:
      .config/nvim/lazy-lock.json
      .config/nvim/lua/plugins/lsp.lua

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -3,19 +3,37 @@ return {
     "neovim/nvim-lspconfig",
     opts = { autoformat = false },
     keys = {
-			{
-			"<C-S>",
-			function()
-				vim.diagnostic.open_float({ border = "rounded" })
-			end,
-			desc = "Line Diagnostics",
-		},
-    }
+      {
+        "<C-S>",
+        function()
+          vim.diagnostic.open_float({ border = "rounded" })
+        end,
+        desc = "Line Diagnostics",
+      },
+    },
   },
   {
     "mason-org/mason.nvim",
     opts = {
       ensure_installed = {},
+    },
+  },
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "InsertEnter",
+    opts = {
+      handler_opts = {
+        border = "rounded",
+      },
+    },
+    keys = {
+      {
+        "<C-s>",
+        function()
+          require("lsp_signature").toggle_float_win()
+        end,
+        { desc = "toggle signature", mode = {"i"} },
+      },
     },
   },
 }
