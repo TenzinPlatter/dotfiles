1d670c67f62c255eb77d191695b3edf50a5b324c
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2026-02-17 21:12:40 +1100

    Modified 1
    Modified:
      .config/nvim/lua/plugins/utils.lua

--- .config/nvim/lua/plugins/utils.lua
+++ .config/nvim/lua/plugins/utils.lua
@@ -5,7 +5,8 @@ return {
   {
     "folke/flash.nvim",
     keys = {
-      { "s", mode = { "n", "x", "o" } },
+      { "s", mode = { "n", "x", "o" }, false },
+      { "gs", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     },
   },
 }
