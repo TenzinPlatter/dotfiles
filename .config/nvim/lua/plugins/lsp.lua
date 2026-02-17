9dab4a3a565675b6b37ac5221ff6e927eb8b2b97
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2026-02-17 12:30:19 +1100

    Modified 1
    Modified:
      .config/nvim/lua/plugins/lsp.lua

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,6 +2,15 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = { autoformat = false },
+    keys = {
+			{
+			"<C-S>",
+			function()
+				vim.diagnostic.open_float({ border = "rounded" })
+			end,
+			desc = "Line Diagnostics",
+		},
+    }
   },
   {
     "mason-org/mason.nvim",
