8ecf37ea2514597e342da43b88ba850f28435fc1
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2026-02-18 12:18:45 +1100

    Modified 1
    Modified:
      .config/nvim/lua/plugins/dev.lua

--- .config/nvim/lua/plugins/dev.lua
+++ .config/nvim/lua/plugins/dev.lua
@@ -4,9 +4,10 @@ return {
     ft = "lua", -- only load on lua files
     opts = {
       library = {
-        -- See the configuration section for more details
         -- Load luvit types when the `vim.uv` word is found
         { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        -- Always load Neovim runtime types (fixes vim.* in ftplugin/ etc.)
+        vim.env.VIMRUNTIME,
       },
     },
   },
