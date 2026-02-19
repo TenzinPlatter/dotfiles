a86c2eac4ad13d172428c8b31d7e784140f89c19
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2026-02-19 15:25:27 +1100

    Squash merge nvim-rewrite into main

--- .config/zsh/.zshrc
+++ .config/zsh/.zshrc
@@ -25,11 +25,12 @@ export ZSH_THEME=""
 export EDITOR="nvim"
 export TERMINAL="kitty"
 export NIXPKGS_ALLOW_UNFREE=1
-export cc="clang"
 
-export PATH="${(j/:/)path}"
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/cuda-13.0/lib64
 export POKEGO_CACHE_AGE=300
+export PATH="${(j/:/)path}"
+
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/cuda-13.0/lib64
+export LD_FLAGS="-fuse-ld=mold"
 
 export MANPAGER='nvim +Man!'   
 
@@ -43,6 +44,11 @@ zle -N edit-command-line
 
 bindkey '^ ' autosuggest-accept
 bindkey '' edit-command-line
+
+# fn to run on change pwd
+chpwd() {
+  set_platform_module
+}
 
 stty -ixon
 
---
+++ .config/zsh/user/fns/ros.zsh
@@ -0,0 +1,31 @@
+sr() {
+    distro="kilted"
+
+    if ! command -v ros2 &> /dev/null; then
+        source /opt/ros/$distro/setup.zsh
+        echo "Sourced global overlay at /opt/ros/$distro"
+    fi
+
+    if [[ ! "$(which rosdep)" = "/home/tenzin/Repositories/rosdep/install/rosdep/bin/rosdep" ]]; then
+        source /home/tenzin/Repositories/rosdep/install/setup.zsh
+        echo "Sourced rosdep fork at /home/tenzin/Repositories/rosdep"
+    fi
+
+    local dir_name="${PWD:t}"
+    if [[ -f /opt/greenroom/$dir_name/install/setup.bash ]]; then
+        source /opt/greenroom/$dir_name/install/setup.bash
+    fi
+
+    if [[ -n "${PLATFORM_MODULE}" && "${PLATFORM_MODULE}" != "${dir_name}" && -f /opt/greenroom/${PLATFORM_MODULE}/install/setup.bash ]]; then
+        source /opt/greenroom/$dir_name/install/setup.bash
+    fi
+
+
+    [[ -f /opt/greeroom/setup.zsh ]] && source /opt/greenroom/setup.zsh
+
+    if [[ -f ./install/setup.zsh ]]; then
+        source ./install/setup.zsh
+        echo "Sourced local workspace"
+    fi
+}
+
