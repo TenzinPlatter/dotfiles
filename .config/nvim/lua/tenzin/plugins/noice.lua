a05c02aa69b0352159d9f832f220253183cefcd1
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2026-02-19 11:03:10 +1100

    Modified 1
    Modified:
      .config/nvim/lua/tenzin/plugins/noice.lua

--- .config/nvim/lua/tenzin/plugins/noice.lua
+++ .config/nvim/lua/tenzin/plugins/noice.lua
@@ -7,7 +7,6 @@ return {
 			override = {
 				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 				["vim.lsp.util.stylize_markdown"] = true,
-				["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
 			},
 		},
 		presets = {
@@ -18,7 +17,7 @@ return {
 			lsp_doc_border = true, -- add a border to hover docs and signature help
 		},
 		popupmenu = {
-			enabled = true,
+			enabled = false,
 			backend = "nui",
 		},
 	},
