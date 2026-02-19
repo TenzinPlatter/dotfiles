caa99b9cf4809400f0d49dcf7be151ed8345e3e0
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2026-02-19 14:48:01 +1100

    Modified 2
    Modified:
      .config/nvim/lazy-lock.json
      .config/nvim/lua/tenzin/plugins/render-markdown.lua

--- .config/nvim/lua/tenzin/plugins/render-markdown.lua
+++ .config/nvim/lua/tenzin/plugins/render-markdown.lua
@@ -1,18 +1,7 @@
 return {
-	"https://github.com/MeanderingProgrammer/render-markdown.nvim",
-	config = function()
-		vim.api.nvim_set_hl(0, "RenderMarkdownCodeBorderSubtle", { bg = "#1a1a1a" })
-
-		require("render-markdown").setup({
-			bullet = {
-				right_pad = 1,
-			},
-			code = {
-				disable_background = true,
-				style = "full",
-				border = "thick",
-				highlight_border = "RenderMarkdownCodeBorderSubtle",
-			},
-		})
-	end,
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.icons" },
+		opts = {},
+	},
 }
