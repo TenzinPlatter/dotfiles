60af5c96c8be33e0597de9ce5f131e6b6ea2aa7d
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2026-03-10 12:15:58 +1100

    Modified 1
    Modified:
      .config/nvim/lua/tenzin/plugins/mini-cursorword.lua

--- .config/nvim/lua/tenzin/plugins/mini-cursorword.lua
+++ .config/nvim/lua/tenzin/plugins/mini-cursorword.lua
@@ -3,5 +3,13 @@ return {
   version = false,
   config = function()
     require("mini.cursorword").setup()
+
+    local function set_hl()
+      vim.api.nvim_set_hl(0, "MiniCursorword", { bg = "#3d3d3d", underline = false })
+      vim.api.nvim_set_hl(0, "MiniCursorwordCurrent", { bg = "#3d3d3d", underline = false })
+    end
+
+    set_hl()
+    vim.api.nvim_create_autocmd("ColorScheme", { callback = set_hl })
   end,
 }
