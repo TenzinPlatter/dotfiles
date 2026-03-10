1d5b232d57107a785d61bf64610fee32449dd603
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2026-03-10 15:48:35 +1100

    Modified 2, Added 1
    Modified:
      .config/nvim/lazy-lock.json
      .config/nvim/lua/tenzin/plugins/mini.lua
    Added:
      .config/nvim/lua/tenzin/plugins/whichkey.lua

--- .config/nvim/lua/tenzin/plugins/mini.lua
+++ .config/nvim/lua/tenzin/plugins/mini.lua
@@ -49,35 +49,6 @@ return {
       notify.setup()
       vim.notify = notify.make_notify()
 
-      -- Clue (replaces which-key.nvim)
-      local miniclue = require("mini.clue")
-      miniclue.setup({
-        triggers = {
-          { mode = "n", keys = "<Leader>" },
-          { mode = "x", keys = "<Leader>" },
-          { mode = "n", keys = "g" },
-          { mode = "x", keys = "g" },
-          { mode = "n", keys = "[" },
-          { mode = "n", keys = "]" },
-          { mode = "n", keys = "z" },
-          { mode = "x", keys = "z" },
-          { mode = "n", keys = "'" },
-          { mode = "n", keys = "`" },
-          { mode = "n", keys = '"' },
-          { mode = "x", keys = '"' },
-          { mode = "i", keys = "<C-r>" },
-          { mode = "n", keys = "<C-w>" },
-        },
-        clues = {
-          miniclue.gen_clues.builtin_completion(),
-          miniclue.gen_clues.g(),
-          miniclue.gen_clues.marks(),
-          miniclue.gen_clues.registers(),
-          miniclue.gen_clues.windows(),
-          miniclue.gen_clues.z(),
-        },
-        window = { delay = 250 },
-      })
 
       -- Sessions (replaces persistence.nvim)
       require("mini.sessions").setup({
