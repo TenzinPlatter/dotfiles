5555b98b18b5c275fcbe380b7920525f4479c050
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2026-03-19 11:45:51 +1100

    Modified 1
    Modified:
      config/nvim/lua/tenzin/plugins/claudecode.lua

--- .config/nvim/lua/tenzin/plugins/claudecode.lua
+++ .config/nvim/lua/tenzin/plugins/claudecode.lua
@@ -4,10 +4,7 @@ return {
   config = true,
   keys = {
     { "<leader>ac", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" },
-    { "<leader>ar", "<cmd>ClaudeCode --resume<cr>", desc = "Resume Claude" },
-    { "<leader>aC", "<cmd>ClaudeCode --continue<cr>", desc = "Continue Claude" },
-    { "<leader>am", "<cmd>ClaudeCodeSelectModel<cr>", desc = "Select Claude model" },
-    { "<leader>ab", "<cmd>ClaudeCodeAdd %<cr>", desc = "Add current buffer" },
+    { "<leader>ap", "<cmd>ClaudeCodeAdd %<cr>", desc = "Add current buffer" },
     { "<leader>as", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send to Claude" },
     {
       "<leader>as",
