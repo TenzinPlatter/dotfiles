2642be307b3924e623f5d2adac70bc956d872045
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2026-04-08 10:46:15 +1000

    Modified 1
    Modified:
      config/nvim/init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,6 @@
 vim.g.mapleader = " "
 vim.opt.termguicolors = true
 
-require("vim._core.ui2").enable()
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
