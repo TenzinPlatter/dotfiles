9bb07d6e9ae21a11b2caa78d60345867abcb76a8
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2026-04-28 12:26:41 -0700

    Modified 1
    Modified:
      config/nvim/lua/tenzin/plugins/render-markdown.lua

--- .config/nvim/lua/tenzin/plugins/render-markdown.lua
+++ .config/nvim/lua/tenzin/plugins/render-markdown.lua
@@ -3,5 +3,9 @@ return {
     "MeanderingProgrammer/render-markdown.nvim",
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.icons" },
     opts = {},
+    enabled = false,
   },
+  {
+    "https://github.com/delphinus/md-render.nvim",
+  }
 }
