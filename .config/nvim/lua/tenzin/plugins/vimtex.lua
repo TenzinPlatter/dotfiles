e5c4bbe40632dc84a5b3a1cb603d0d9065438ae7
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2026-06-09 20:41:34 +1000

    Modified 1
    Modified:
      config/nvim/lua/tenzin/plugins/vimtex.lua

--- .config/nvim/lua/tenzin/plugins/vimtex.lua
+++ .config/nvim/lua/tenzin/plugins/vimtex.lua
@@ -2,7 +2,7 @@ return {
   "lervag/vimtex",
   lazy = false,
   init = function()
-    vim.g.vimtex_view_method = "sioyek"
+    vim.g.vimtex_view_method = "zathura"
     vim.g.vimtex_syntax_enabled = 0
   end
 }
