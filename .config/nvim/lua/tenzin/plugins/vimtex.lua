7dc42b28807d3f5792d366b28123559b63350d74
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2026-06-10 19:22:38 +1000

    Modified 1
    Modified:
      config/nvim/lua/tenzin/plugins/vimtex.lua

--- .config/nvim/lua/tenzin/plugins/vimtex.lua
+++ .config/nvim/lua/tenzin/plugins/vimtex.lua
@@ -3,6 +3,7 @@ return {
   lazy = false,
   init = function()
     vim.g.vimtex_view_method = "zathura"
-    vim.g.vimtex_syntax_enabled = 0
+    -- Uninstall latex treesitter parser to not conflict with this
+    vim.g.vimtex_syntax_enabled = 1
   end
 }
