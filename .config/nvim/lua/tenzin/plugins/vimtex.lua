230f96973ae7f11049ab08b0c8c50bff2a07cc7d
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2026-06-10 21:18:30 +1000

    Modified 1
    Modified:
      config/nvim/lua/tenzin/plugins/vimtex.lua

--- .config/nvim/lua/tenzin/plugins/vimtex.lua
+++ .config/nvim/lua/tenzin/plugins/vimtex.lua
@@ -5,5 +5,7 @@ return {
     vim.g.vimtex_view_method = "zathura"
     -- Uninstall latex treesitter parser to not conflict with this
     vim.g.vimtex_syntax_enabled = 1
+    -- Don't auto-open qf list when there are compile errors/warnings
+    vim.g.vimtex_quickfix_mode = 0
   end
 }
