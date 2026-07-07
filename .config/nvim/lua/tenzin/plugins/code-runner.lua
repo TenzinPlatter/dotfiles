69739bc4acbcfdaeeb134adac7c78912fc7bf9b5
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2026-07-07 13:44:20 -0400

    Modified 1
    Modified:
      config/nvim/lua/tenzin/plugins/code-runner.lua

--- .config/nvim/lua/tenzin/plugins/code-runner.lua
+++ .config/nvim/lua/tenzin/plugins/code-runner.lua
@@ -1,5 +1,13 @@
 return {
   dir = "/home/tenzin/code/code-runner.nvim/",
+  keys = {
+    {
+      "<leader>cr",
+      function ()
+        vim.cmd("CodeRunner")
+      end
+    }
+  },
   -- opts = {
   --   window = {
   --     split = "right"
